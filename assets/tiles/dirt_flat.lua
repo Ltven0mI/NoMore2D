67239887448ded5df662b78494da7b77223ca98f
Ltven0mI <lt.ven0mi@gmail.com>
2014-11-26 18:16:32 +1000

    Added a new image. Also change the collision for grass, only temporary.

---
+++ assets/tiles/dirt_flat.lua
@@ -0,0 +1,6 @@
+dirt_flat = {}
+dirt_flat.name = "dirt_flat"
+dirt_flat.imageKey = "dirt_flat"
+dirt_flat.collision = false
+
+return dirt_flat
--- assets/tiles/grass_flat.lua
+++ assets/tiles/grass_flat.lua
@@ -1,6 +1,6 @@
 grass_flat = {}
 grass_flat.name = "grass_flat"
 grass_flat.imageKey = "grass_flat"
-grass_flat.collision = false
+grass_flat.collision = true
 
 return grass_flat
