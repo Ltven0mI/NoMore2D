8ce72094db54591c4434cf966a44534a69a52626
Ltven0mI <lt.ven0mi@gmail.com>
2014-11-27 15:06:59 +1000

    Added temporary interaction function in grass_flat.lua tileData

--- assets/tiles/grass_flat.lua
+++ assets/tiles/grass_flat.lua
@@ -3,4 +3,9 @@ grass_flat.name = "grass_flat"
 grass_flat.imageKey = "grass_flat"
 grass_flat.collision = true
 
+-- Functions --
+function grass_flat:interact()
+	self.collision = not self.collision
+end
+
 return grass_flat
