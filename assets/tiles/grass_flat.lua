1b7155c12ca0bf8b419914a90012c81c30869c0f
Ltven0mI <lt.ven0mi@gmail.com>
2014-11-27 20:27:09 +1000

    Removed temporary "grass_flat:interact()"

--- assets/tiles/grass_flat.lua
+++ assets/tiles/grass_flat.lua
@@ -1,11 +1,6 @@
 grass_flat = {}
 grass_flat.name = "grass_flat"
 grass_flat.imageKey = "grass_flat"
-grass_flat.collision = true
-
--- Functions --
-function grass_flat:interact()
-	self.collision = not self.collision
-end
+grass_flat.collision = false
 
 return grass_flat
