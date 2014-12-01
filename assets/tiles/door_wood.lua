7b74813f83b1616c41e4edbd80c8c8a3d1349731
Ltven0mI <lt.ven0mi@gmail.com>
2014-12-01 14:58:29 +1000

    The temporary door now uses the new animation system.

--- assets/tiles/door_wood.lua
+++ assets/tiles/door_wood.lua
@@ -1,9 +1,12 @@
 door_wood = {}
 door_wood.name = "door_wood"
-door_wood.imageKey = "door_shut"
+door_wood.imageKey = "door_wood"
 door_wood.collision = true
 door_wood.collider = {x=0,y=0,w=1,h=0.2}
-door_wood.isFloor = false
+
+door_wood.segments = {x=1,y=3}
+door_wood.playback = "reverseplay"
+door_wood.speed = 10
 
 door_wood.open = false
 
@@ -11,11 +14,11 @@ door_wood.open = false
 function door_wood:interact()
 	self.open = not self.open
 	if self.open then
-		self.imageKey = "door_open"
 		self.collision = false
+		self.playback = "play"
 	else
-		self.imageKey = "door_shut"
 		self.collision = true
+		self.playback = "reverseplay"
 	end
 end
 
