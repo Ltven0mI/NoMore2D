fac870f4c7677e1f2425737a3d1ed65e40db9297
Ltven0mI <lt.ven0mi@gmail.com>
2014-12-09 15:26:49 +1000

    Changed grass_dirt_01 tile a bit.

--- assets/tiles/nature/grass_dirt_01.lua
+++ assets/tiles/nature/grass_dirt_01.lua
@@ -3,6 +3,6 @@ grass_dirt_01.name = "grass_dirt_01"
 grass_dirt_01.imageKey = "grass_dirt_01"
 grass_dirt_01.collision = false
 
-grass_dirt_01.tileset = {x=6,y=3}
+grass_dirt_01.tileset = {x=6,y=6}
 
 return grass_dirt_01
