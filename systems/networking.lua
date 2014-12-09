4e0fc2258685eb1bacc71fac904eb16073e27d5a
Jasper Riedel <jasperandbuddy@gmail.com>
2014-12-10 07:48:58 +1000

    Added callbacks and comments

--- systems/networking.lua
+++ systems/networking.lua
@@ -1,7 +1,15 @@
+-- system setup --
 networking = {}
 networking.systemKey = "networking"
 networking.runPriority = 99
 
+-- love callbacks --
+function networking.load()
+	
+end
 
+function networking.update(dt)
+	
+end
 
 return networking
