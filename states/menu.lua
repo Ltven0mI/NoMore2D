ea1699a8ad9e4f3063df552c93a12005dda4bb9c
Ltven0mI <lt.ven0mi@gmail.com>
2014-12-27 16:52:03 +1000

    Added Multiplayer button.

--- states/menu.lua
+++ states/menu.lua
@@ -4,6 +4,7 @@ menu = {}
 function menu.drawscreen()
 	local bw, bh = main.width/10, main.width/30
 	if ui.button(camera.vWindow.w/2-bw/2, camera.vWindow.h/2-bh/2, bw, bh, "Play") then state.loadState("game") end
+	if ui.button(camera.vWindow.w/2-bw/2, camera.vWindow.h/2-bh/2+bh+5, bw, bh, "Multiplayer") then state.loadState("game") end
 end
 
 function menu.stateOpen()
