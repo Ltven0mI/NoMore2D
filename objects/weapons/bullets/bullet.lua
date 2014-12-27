2b51b07628da1d49fe2690741ebbe5160f76b28b
Ltven0mI <lt.ven0mi@gmail.com>
2014-12-27 17:36:30 +1000

    Moved bullet.lua

--- objects/weapons/bullets/bullet.lua
+++ objects/weapons/bullets/bullet.lua
@@ -4,22 +4,22 @@ bullet.name = "bullet"
 bullet.pos = {x=0,y=0}
 bullet.vel = {x=0,y=0}
 bullet.rot = 0
-bullet.size = {w=15,h=20}
-bullet.speed = 400
+bullet.size = {w=15,h=60}
+bullet.speed = 800
 bullet.life = 4
 bullet.curLife = 0
 
 -- Callbacks --
 function bullet:update(dt)
-	self:setVel(math.sin(math.rad(self.rot))*self.speed, -math.cos(math.rad(self.rot))*self.speed)
-	self.pos.x = self.pos.x + self.vel.x*dt
-	self.pos.y = self.pos.y + self.vel.y*dt
+	local velx, vely = math.sin(math.rad(self.rot))*self.speed, -math.cos(math.rad(self.rot))*self.speed
+	self.pos.x = self.pos.x + (self.vel.x+velx)*dt
+	self.pos.y = self.pos.y + (self.vel.y+vely)*dt
 	self.curLife = self.curLife + dt
 	if self.curLife >= self.life then object.destroyObject(self) end
 end
 
 function bullet:drawworld()
-	ui.draw(image.getImage("bullet_01"), math.round(self.pos.x), math.round(self.pos.y), self.size.w, self.size.h, self.rot)
+	ui.draw(image.getImage("bullet_02"), math.round(self.pos.x), math.round(self.pos.y), self.size.w, self.size.h, self.rot)
 end
 
 -- Functions --
